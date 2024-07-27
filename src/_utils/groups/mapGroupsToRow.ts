05ad9f176d4a971a407aa8ffb0c8dba0c470b7e7
asapovk <asapovk@gmail.com>
2024-07-27 22:16:21 +0300

    fix mappers

--- src/_utils/groups/mapGroupsToRow.ts
+++ src/_utils/groups/mapGroupsToRow.ts
@@ -2,8 +2,10 @@ import { ResQType } from '../../_api/_reqTypes';
 import { IGroupRow } from '../../_interfaces/groups/IGroupsRow.interface';
 
 export const mapGroupsToRow = (data: ResQType<'groups'>): Array<IGroupRow> =>
-  data.groups.map((c) => ({
-    dtCreate: c.dtCreate,
-    groupId: c.groupId,
-    name: c.groupName,
-  }));
+  data?.groups
+    ? data.groups.map((c) => ({
+        dtCreate: c.dtCreate,
+        groupId: c.groupId,
+        name: c.groupName,
+      }))
+    : [];
