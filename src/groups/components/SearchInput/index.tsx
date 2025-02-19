deae9a193247df403fe6baf68c09704f40436ade
asapovk <asapovk@gmail.com>
2025-02-19 23:45:28 +0300

    styled search2

--- src/groups/components/SearchInput/index.tsx
+++ src/groups/components/SearchInput/index.tsx
@@ -4,9 +4,16 @@ import { useTrigger } from '@reflexio/react-v1/lib/useTrigger';
 import { useReflector } from '@reflexio/react-v1/lib/useReflector';
 import { _IState, _ITriggers } from '../../../_redux/types';
 import style from './search-styles.module.less';
+import { Expand, Search } from 'lucide-react';
 
 export const SearchInput = () => (
   <div className={style.root}>
-    <input />
+    <div className={style.icon}>
+      <Search />
+    </div>
+    <textarea />
+    <div className={style.icon}>
+      <Expand />
+    </div>
   </div>
 );
